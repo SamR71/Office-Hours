52e87b89e8aad6e3bdf8c74bb6ec56879c5ddf34
Kirsten <martindale29k@gmail.com>
2020-02-06 08:39:35 -0500

    Created admin login for backend and configured backend todo

--- Uh-OH!/backend/todo/admin.py
+++ Uh-OH!/backend/todo/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Todo 
+
+class TodoAdmin(admin.ModelAdmin):
+	list_display = ('title', 'description', 'completed')
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
--- Uh-OH!/backend/todo/models.py
+++ Uh-OH!/backend/todo/models.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
 # Create your models here.
+
+class Todo(models.Model):
+	title = models.CharField(max_length=120)
+	description = models.TextField()
+	completed = models.BooleanField(default=False)
+
+	def _str_(self):
+		return self.title
