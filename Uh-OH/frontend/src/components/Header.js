777ff4748ebae57f3d6b8f9a72c1e2668eb0339f
kirm99 <martindale29k@gmail.com>
2020-02-15 17:23:21 -0500

    made logo link to the home page

--- Uh-OH/frontend/src/components/Header.js
+++ Uh-OH/frontend/src/components/Header.js
@@ -1,30 +1,61 @@
 import React from 'react';
 import logo from './UhOhLogo.png';
+import {withRouter} from 'react-router-dom'
 
 
-class Header extends React.Component{
-    render() {
+class Header extends React.Component
+{
+    constructor(props)
+    {
+        super(props);
 
-    	const headerStyle = {
+        this.redirectHome = this.redirectHome.bind(this);
+        this.redirectLogIn = this.redirectLogIn.bind(this);
+        this.redirectSearch = this.redirectSearch.bind(this);
+    }
+
+    redirectHome()
+    {
+        this.props.history.push('/')
+    }
+
+    redirectLogIn()
+    {
+        this.props.history.push('/LogIn')
+    }
+
+    redirectSearch()
+    {
+        this.props.history.push('/Search')
+    }
+
+    render()
+    {
+        const headerStyle =
+        {
 		    backgroundColor: "Grey",
 		    padding: "5px",
 			overflow: "hidden",
 	    };
 
-    	const logoStyle = {
+    	const logoStyle =
+        {
     	    float: "left",
             display: "block",
             marginLeft: "20px",
+            href: "/",
         };
 
-    	const searchBarStyle = {
+    	const searchBarStyle =
+        {
             width: "70%",
             marginTop: "20px",
             marginLeft: "100px",
             padding: "5px",
         };
 
-        const logInStyle = {
+        const logInStyle =
+        {
             float: "right",
             display: "block",
             marginRight: "50px",
@@ -33,14 +64,19 @@ class Header extends React.Component{
 
         return (
             <div style={headerStyle}>
-                <img style={logoStyle} src={logo} alt="Uh-OH! Logo" height="75" width="75" />
-                <button style={logInStyle} type="button" className="btn btn-light">Log-In</button>
+                <img onClick={this.redirectHome} style={logoStyle} src={logo} alt="Uh-OH! Logo" height="75" width="75" />
+                <button onClick={this.redirectLogIn} style={logInStyle} type="button" className="btn btn-light">Log-In</button>
             	<form>
-					<input style={searchBarStyle} type="text" placeholder="Search for your courses ..." name="course search bar" />
+					<input onClick={this.redirectSearch}
+                           style={searchBarStyle}
+                           type="text"
+                           placeholder="Search for your courses ..."
+                           name="course search bar"
+                    />
 				</form>
 			</div>
         );
     }
 }
 
-export default Header;
+export default withRouter(Header);
--- Uh-OH/frontend/src/components/Home.js
+++ Uh-OH/frontend/src/components/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-class Home extends React.Component{
-    render() {
+class Home extends React.Component
+{
+    render()
+    {
 
-        const homePageStyle = {
+        const homePageStyle =
+        {
             margin: "20px",
             fontFamily: "sans-serif",
-        }
+        };
 
-        return (
+        return(
             <div style={homePageStyle}>
                 <h1>My Schedule:</h1>
                 <p>Put schedule component here when done</p>
