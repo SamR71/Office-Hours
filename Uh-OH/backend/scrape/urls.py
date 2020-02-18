1feb9ccef063a752ca665a2d60fd077894009a3a
Sam Rackey <samrcr71@gmail.com>
2020-02-18 12:38:02 -0500

    -added api to search for classes via class name
    Signed-off-by: Sam Rackey <samrcr71@gmail.com>

--- Uh-OH/backend/scrape/urls.py
+++ Uh-OH/backend/scrape/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+	path('courses/',views.CourseAPIView.as_view())
 ]
--- Uh-OH/backend/scrape/views.py
+++ Uh-OH/backend/scrape/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework import generics
+from rest_framework import filters
+
+from .models import Course
+from .serializers import CourseSerializer
+
+
 # Create your views here.
 def home(request):
     return render(request, template_name='base.html')
 
+class CourseAPIView(generics.ListCreateAPIView):
+    search_fields = ['courseName']
+    filter_backends = (filters.SearchFilter,)
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
