2c8ebe93ee44f011191cc470988ac6b8209418ef
kirm99 <martindale29k@gmail.com>
2020-02-20 13:03:59 -0500

    finshed schedule display we will need to define our own event component to add office hours to it

--- Uh-OH/frontend/src/components/Schedule.js
+++ Uh-OH/frontend/src/components/Schedule.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import moment from 'moment';
 import WeekCalendar from 'react-week-calendar';
-import 'react-week-calendar/dist/style.css';
+import './ScheduleStyle.css';
 
 class Schedule extends React.Component
 {
+
     render()
     {
 
@@ -12,13 +13,15 @@ class Schedule extends React.Component
             <div>
                 <WeekCalendar
                     firstDay = {moment().day(1)}
-                    startTime = {moment({h: 8, m: 0})}
-                    endTime = {moment({h: 20, m: 0})}
+                    startTime = {moment({h:8, m:0})}
+                    endTime = {moment({h:20, m:0})}
                     scaleUnit = {60}
+                    scaleFormat = {"h:mm a"}
                     dayFormat = {'dd.'}
-                    cellHeight = {50}
+                    cellHeight = {40}
                     numberOfDays = {7}
                     useModal = {false}
+                    /*eventComponent = will take in a component we define to display specific hours*/
                     />
             </div>
         );
