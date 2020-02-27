163ac02610c247bc84f1ddc8f35355d70bb7392a
rsdexter <rsdexter00@gmail.com>
2020-02-27 14:49:30 -0500

    added meeting time serializer and updated course/section serializers

--- Uh-OH/backend/scrape/serializers.py
+++ Uh-OH/backend/scrape/serializers.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
-from .models import Course, CourseSection
+from .models import Course, CourseSection, CourseMeetingTime
+
+class MeetingTimeSerializer(serializers.ModelSerializer):
+	coursesection_id = serializers.PrimaryKeyRelatedField(queryset=CourseSection.objects.all(),source='meetSection.id')
+	class Meta:
+		model = CourseMeetingTime
+		fields = 	('id',
+					'meetSection',
+					'meetType',
+					'meetDates',
+					'meetStartTime',
+					'meetEndTime',
+					'meetInstructor',
+					'coursesection_id')
 
 class CourseSectionSerializer(serializers.ModelSerializer):
 	course_id = serializers.PrimaryKeyRelatedField(queryset=Course.objects.all(),source='currentCourse.id')
+	meetingTimes = MeetingTimeSerializer(many=True, read_only=True)
 	class Meta:
 		model = CourseSection
-		fields = ('id', 'currentCourse', 'sectionID', 'course_id')
+		fields = ('id', 'currentCourse', 'sectionID', 'course_id', 'meetingTimes')
 
 class CourseSerializer(serializers.ModelSerializer):
 	sections = CourseSectionSerializer(many=True, read_only=True)
 	class Meta:
 		model = Course
-		fields = ('id', 'courseName', 'sections')
+		fields = ('id', 'courseName', 'courseValue', 'courseAbbrev', 'sections')
