ba71f9c84118c4aadc71b011697104230995d252
Sam Rackey <samrcr71@gmail.com>
2020-03-04 20:45:36 -0500

    -styled login/signup with bootstrap
    Signed-off-by: Sam Rackey <samrcr71@gmail.com>

--- Uh-OH/frontend/src/components/SignIn/SignUp.js
+++ Uh-OH/frontend/src/components/SignIn/SignUp.js
@@ -31,23 +31,45 @@ class SignUp extends React.Component{
 
     render(){
         return(
-            <div className="signup">
-                <form>
-                    <label htmlFor="fullName"><b>Full Name: </b></label>
-                    <input type="text" onChange={this.handleChange} placeholder="Full Name" name="fullName" required />
-                    <br/>
-                    <label htmlFor="email"><b>Email address: </b></label>
-                    <input type="text" onChange={this.handleChange} placeholder="Email address" name="email" required />
-                    <br/>
-                    <label htmlFor="password"><b>Password: </b></label>
-                    <input type="password" onChange={this.handleChange} placeholder="password" name="password" required />
-                    <br/>
-                    <label htmlFor="repeatPassword"><b>Renter Password: </b></label>
-                    <input type="password" onChange={this.handleChange} placeholder="password" name="repeatPassword" required />
-                    <br/>
-                    <button className="signinbutton" onClick={this.handleClick}>Sign-up</button>
-                </form>
-            </div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-5 mx-auto">
+						<div class="myform form">
+							<div class="logo mb-3">
+								<div class="col-md-12 text-center">
+									<h1 >Signup</h1>
+								</div>
+							</div>
+							<form action="#" name="registration">
+								<div class="form-group">
+									<label for="exampleInputEmail1">Name</label>
+									<input type="text"  name="fullName" class="form-control" id="fullName" aria-describedby="emailHelp" placeholder="FullName" onChange={this.handleChange} />
+								</div>
+								<div class="form-group">
+									<label for="exampleInputEmail1">Email</label>
+									<input type="email"  name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter Lastname" onChange={this.handleChange} />
+								</div>
+								<div class="form-group">
+									<label for="exampleInputEmail1">Password</label>
+									<input type="password" name="password"  class="form-control" id="password" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.handleChange}/>
+								</div>
+								<div class="form-group">
+									<label for="exampleInputEmail1">Confirm Password</label>
+									<input type="password" name="repeatPassword" id="repeatPassword"  class="form-control" aria-describedby="emailHelp" placeholder="Enter Password" onChange={this.handleChange}/>
+								</div>
+								<div class="col-md-12 text-center mb-3">
+									<button  onClick={this.handleClick} class=" btn btn-block mybtn btn-primary tx-tfm">Join Uh-OH!</button>
+								</div>
+								<div class="col-md-12 ">
+									<div class="form-group">
+										<p class="text-center"><a href="/LogIn/" id="signin">Already have an account?</a></p>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
         )
     }
 }
