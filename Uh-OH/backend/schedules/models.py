e867ea42cb6d91c515d85a639da803874a0a8223
Sam Rackey <samrcr71@gmail.com>
2020-03-28 14:32:51 -0400

    added a model representing a single schedule item for a user
    Signed-off-by: Sam Rackey <samrcr71@gmail.com>

--- Uh-OH/backend/schedules/models.py
+++ Uh-OH/backend/schedules/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 
 # Create your models here.
-class userSchedule(models.Model):
-    user = models.TextField()
-    courses = models.TextField
+class userScheduleItem(models.Model):
+	"""
+	CourseSection Class represents a single section of a Course
+	by the Course, sectionID, and instructorName.
+	"""
+	class Meta:
+		verbose_name = 'User Schedule Item'
+		verbose_name_plural = 'User Schedule Items'
+    
+	#for whom is this an entry in their schedule
+	username = models.CharField(max_length=50)
+	
+	
+	#information about the given entry
+	meetInstructor = models.CharField(max_length=50)
+	meetStartTime = models.CharField(max_length=7)
+	meetEndTime = models.CharField(max_length=7)
+	meetLocation = models.CharField(max_length=23)
+	meetDates = models.CharField(max_length=7)
+
+	def __str__(self):
+		return str(self.Instructor) + " + " + str(self.meetLocation) + " + " + str(self.meetDates) + " + " + str(self.meetStartTime) + " + " + str(self.meetEndTime)
