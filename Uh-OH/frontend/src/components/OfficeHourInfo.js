66bec424f3198afb4bce123cd83f4f98ab125f0b
rsdexter <rsdexter00@gmail.com>
2020-03-29 19:23:54 -0400

    added and styled an 'add' button for each office hour

--- Uh-OH/frontend/src/components/OfficeHourInfo.js
+++ Uh-OH/frontend/src/components/OfficeHourInfo.js
@@ -20,8 +20,12 @@ class OfficeHourInfo extends React.Component {
 	render() {
 		return (
 			<div>
-			{/* The Body Content of the OfficeHourInfo component */}
-			{this.state.dates + " | " + this.state.location + " | " + this.state.startTime + "-" + this.state.endTime}
+				{/* The Body Content of the OfficeHourInfo component */}
+				{this.state.dates + " | " + this.state.location + " | " + this.state.startTime + "-" + this.state.endTime + " "}
+				{/* For more info on customizing and adding functionality:
+					https://react-bootstrap.github.io/components/buttons/ */}
+				{/* The button to add a specific office hours to the user schedule, currently does nothing */}
+				<button type="button" class="btn btn-sm btn-outline-primary">Add</button>
 			</div>
 		);
 	}
