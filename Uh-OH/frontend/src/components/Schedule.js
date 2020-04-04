24d71ce3c06c8e1397a75891fabec7ee2cff05cf
kirm99 <martindale29k@gmail.com>
2020-04-04 14:35:11 -0400

    got office hours to display on schedule, still need instructor information and class name intformation

--- Uh-OH/frontend/src/components/Schedule.js
+++ Uh-OH/frontend/src/components/Schedule.js
@@ -7,59 +7,49 @@ import "./ScheduleStyle.css";
 class Schedule extends React.Component
 {
 
+    state = {
+        loggedin: ""
+    };
+    
     constructor(props)
 	{
         super(props);
         this.state =
-        {
-        	test: "",
+	    {
             rawData: [],
             eventIntervals: [],
         };
         this.helpFormatStrings = this.helpFormatStrings.bind(this);
+        this.numDay = this.numDay.bind(this);
     }
 
     async componentDidMount()
 	{
+        // Retrieve login token
+        var user = localStorage.getItem("loggedinuser");
+        
 		// GET request to get the schedule data.
-        // Schedule will be stored in the string 'schedule'
-        let schedule = "testing";
+        let schedule = null;
         let url = "http://localhost:8000/schedules/get/";
         let xhr = new XMLHttpRequest();
 
-        xhr.onreadystatechange = function()
-        {
-            if (xhr.readyState === XMLHttpRequest.DONE)
-            {
-                let status = xhr.status;
-                if (status === 0 || (status >= 200 && status < 400))
-                {
-                    schedule = xhr.responseText;
-                }
-                else
-                {
-                    schedule = "ERROR!"
-                }
-            }
-        };
-
-        //tried to get schedule to say something other than testing this also didnt work
         // get a callback when the server responds
-	    //xhr.addEventListener("load", () => {
+	    xhr.addEventListener("load", () => {
 	        // update the state of the component with the result here
-	    //    schedule = xhr.responseText;
-	    //})
+	        schedule = xhr.responseText;
+		    this.setState({rawData: schedule.split(",")});
+		    this.helpFormatStrings();
+	    });
 
         xhr.open("POST", url);
         const form = new FormData();
 
         // Send along login token
-        form.set("user",this.state.loggedin);
+        form.set("user",user);
         xhr.send(form);
-
-		this.setState({test: schedule});
 	}
 
+	// this helper functoin takes the raw string data and converts to event interval object
 	helpFormatStrings()
 	{
 		// clear intervals of any old data
@@ -68,19 +58,45 @@ class Schedule extends React.Component
 		let intervals = [];
 		let officeHour = "";
 		//looping over all office hours to turn into intervals
-		for(officeHour in this.state.rawData)
+		for(let i = 1; i < this.state.rawData.length; i++)
 		{
+			officeHour = this.state.rawData[i];
 			const info = officeHour.split(" + ");
 			const days = info[2].split("");
 
-			const start = moment(info[3], "LT");
-			const end = moment(info[4], "LT");
-			const value = info[1];
+			for(let j = 0; j < days.length; j++)
+			{
+				const start = moment(info[3], "LT").day(this.numDay(days[j]));
+				const end = moment(info[4], "LT").day(this.numDay(days[j]));
+				const value = info[1];
 
-			intervals.push({start, end, value});
+				intervals.push({start, end, value});
+			}
 		}
 
 		this.setState({eventIntervals: intervals})
+	}
+
+	// this helper function takes in the charcter for a day and trurns the number for it for moments
+	numDay(day)
+	{
+		switch(day)
+		{
+			case "M":
+				return 1;
+			case "T":
+				return 2;
+			case "W":
+				return 3;
+			case "R":
+				return 4;
+			case "F":
+				return 5;
+			case "S":
+				return 6;
+			default:
+				return 7;
+		}
 	}
 
     render()
@@ -88,8 +104,6 @@ class Schedule extends React.Component
 
         return(
             <div>
-	            {this.state.rawData}
-	            {this.state.test}
                 <WeekCalendar
                     firstDay = {moment().day(1)}
                     startTime = {moment({h:8, m:0})}
