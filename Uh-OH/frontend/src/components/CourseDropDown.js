c3f0ff02c8a4bf6a92dcf83dd5e67cbdd46ad4ce
rsdexter <rsdexter00@gmail.com>
2020-04-08 16:24:46 -0400

    Added documentation to CourseDropDown

--- Uh-OH/frontend/src/components/CourseDropDown.js
+++ Uh-OH/frontend/src/components/CourseDropDown.js
@@ -2,7 +2,14 @@ import React from 'react';
 import SectionModal from "./SectionModal";
 import {withRouter} from 'react-router-dom';
 
+/* The CourseDropDown component displays the sections of a course
+ * as a drop down menu. When the course is clicked, the dropdown menu
+ * is toggled to show every section of the respective course.
+ */
 class CourseDropDown extends React.Component {
+	/* The state holds the name of the Course, as well as two arrays of
+	 * the Intructors and CourseSections that belongs to the Course. 
+	*/
 	state = {
 		name: "",
 		sections: [],
@@ -18,7 +25,8 @@ class CourseDropDown extends React.Component {
 	render() {
 		return (
 			<div>
-				{/* The DropDown button labeled as the name of the course */}
+				{/* The DropDown button is labeled as the name of the course 
+					and assigned to toggle the collapsable dropdown menu */}
 				<a  class="btn btn-link"
 					data-toggle="collapse"
 					href={"#collapseCourse"+this.state.name}
@@ -27,10 +35,13 @@ class CourseDropDown extends React.Component {
 					{this.state.name}
 				</a>
 				<br></br>
-				{/* The DropDown Content that displays each section of the course */}
+				{/* The DropDown Content that displays each section of the course.
+					Each SectionModal must be given the name of the course as well as
+					the CourseSection class itself and the array of instructors. */}
 				<div class="collapse" id={"collapseCourse"+this.state.name}>
 					<div class="card card-body">
-						{this.state.sections.map(item => (
+						{	//Each section is mapped to a <div> class with its ID as the key, and mapped to a SectionModal
+							this.state.sections.map(item => (
 							<div key={item.id}>
 								<SectionModal name={this.state.name} section={item} instructors={this.state.instructors} loggedin={this.props.loggedin}/>
 							</div>
