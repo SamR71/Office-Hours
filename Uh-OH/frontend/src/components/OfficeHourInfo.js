2d410fcd3d7f4389c845fc3b3f69d70c1ef88bd0
rsdexter <rsdexter00@gmail.com>
2020-04-08 16:59:38 -0400

    Added documentation for the OfficeHourInfo components

--- Uh-OH/frontend/src/components/OfficeHourInfo.js
+++ Uh-OH/frontend/src/components/OfficeHourInfo.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import {withRouter} from 'react-router-dom';
 
+/* The OfficeHourInfo component displays information about an InstructorOfficeHour object.
+ * It displays the information as a string but can be modified. It also handles adding
+ * the current office hour to the user's schedule
+ */
 class OfficeHourInfo extends React.Component {
+	// the state holds information from the fields of the InstructorOfficeHour class
+	// as well as loggedin to determine the account the user is logged in to
 	state = {
 		dates: "",
 		location: "",
@@ -17,14 +23,17 @@ class OfficeHourInfo extends React.Component {
 					startTime: props.officeHour.meetStartTime,
 					endTime: props.officeHour.meetEndTime,
                     instructor: props.officeHour.meetInstructor};
+        // initialize the handleClick function to allow it to execute
         this.handleClick = this.handleClick.bind(this);
 	}
     
+    // adds the loggedin information to the state
     componentDidMount(){
 		var user = localStorage.getItem('loggedinuser');
 		this.setState({loggedin: user});
     }
 
+    // adds the current office hour to the schedule of the logged in user
     addToSchedule(){
         var url = 'http://localhost:8000/schedules/add/';
         var xhr = new XMLHttpRequest()
@@ -35,7 +44,9 @@ class OfficeHourInfo extends React.Component {
         }
         xhr.onreadystatechange = function() {
         }
+        // open a post request at the specified url
         xhr.open('POST', url)
+        // create a form with the office hour information
 		const form = new FormData()
 		form.set('dates', this.state.dates)
 		form.set('location', this.state.location)
@@ -43,9 +54,11 @@ class OfficeHourInfo extends React.Component {
 		form.set('endTime', this.state.endTime)
         form.set('instructor', this.state.instructor)
         form.set('user',this.state.loggedin)
+        // send the form data to the post request
         xhr.send(form)
     }
     
+    // executes on the button click to call the addToSchedule() function
 	handleClick(event)
     {
         event.preventDefault();
@@ -59,7 +72,7 @@ class OfficeHourInfo extends React.Component {
 				{this.state.dates + " | " + this.state.location + " | " + this.state.startTime + "-" + this.state.endTime + " "}
 				{/* For more info on customizing and adding functionality:
 					https://react-bootstrap.github.io/components/buttons/ */}
-				{/* The button to add a specific office hours to the user schedule, currently does nothing */}
+				{/* The button to add this office hour to the user schedule */}
 				<button onClick={this.handleClick} type="button" class="btn btn-sm btn-outline-primary">Add</button>
 			</div>
 		);
