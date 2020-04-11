56d5867aca2ecd6c049b02aa5893449bd6fe9456
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-04-11 14:55:13 -0400

    Initial Incomplete Backend Framework For Updating Office Hours.

--- Uh-OH/backend/scrape/urls.py
+++ Uh-OH/backend/scrape/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 	path('courses/',views.CourseAPIView.as_view())
+	#URL Path For Updating Office Hours 
+	path('update/', views.UpdateOHAPIView.as_view())
 ]
--- Uh-OH/backend/scrape/views.py
+++ Uh-OH/backend/scrape/views.py
@@ -11,10 +11,22 @@ from .serializers import CourseSerializer
 def home(request):
     return render(request, template_name='base.html')
 
-#CourseAPIView Is The View Invoked By The Frontend To 
-#Return All Courses Present For The Main Search Page.
+#CourseAPIView Is The View Invoked By The Frontend 
+#To Return All Courses Present For The Main Search Page.
 class CourseAPIView(generics.ListCreateAPIView):
     search_fields = ['courseName', 'courseAbbrev', 'courseValue']
     filter_backends = (filters.SearchFilter,)
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+#UpdateOHAPIView Is The View Invoked By The Frontend
+#To Update A Specific Office Hours Section.
+class UpdateOHAPIView(object):
+	"""docstring for UpdateAPIView"""
+	#Note: Need To Figure Out How Frontend Is Going To Send 
+	#The Office Hours Section They Are Trying To Modify 
+	# + The New Data For The Office Hours.
+	def __init__(self, arg):
+		super(UpdateAPIView, self).__init__()
+		self.arg = arg
+		
