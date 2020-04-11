f5b5adc3ff186ed43c9d473c746d2df0ae84a231
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-04-11 17:26:14 -0400

    Implemented Backend Support To Update InstructorOfficeHours Based On Frontend Post Request.

--- Uh-OH/backend/scrape/urls.py
+++ Uh-OH/backend/scrape/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
 	path('courses/',views.CourseAPIView.as_view()),
-	path('hours/', views.InstructorAPIView.as_view())
+	path('hours/', views.InstructorAPIView.as_view()),
+	path('update/', views.InstructorOHAPIView.update, name="InstructorOHAPIView.update")
 ]
--- Uh-OH/backend/scrape/views.py
+++ Uh-OH/backend/scrape/views.py
@@ -5,9 +5,9 @@ from rest_framework import filters
 
 from .models import Course
 from .models import Instructor
+from .models import InstructorOfficeHours
 from .serializers import CourseSerializer
 from .serializers import InstructorSerializer
-
 
 # Create your views here.
 def home(request):
@@ -37,5 +37,38 @@ class InstructorAPIView(generics.ListCreateAPIView):
 	#Note: Need To Figure Out How Frontend Is Going To Send 
 	#The Office Hours Section They Are Trying To Modify 
 	# + The New Data For The Office Hours.
-	
+class InstructorOHAPIView(object):
+	"""docstring for UpdateOHAPIView"""
+	def __init__(self, arg):
+		super(UpdateOHAPIView, self).__init__()
+		self.arg = arg
+		
+	@api_view(['POST'])
+	@parser_classes([MultiPartParser, FormParser])
+	@csrf_exempt
+	def update(request):
+		#Get Old InstructorOfficeHours Attributes:
+		oldID = request.data.get("oldID")
+		#The Other Old Attributes Are As Follows:
+		#	oldStartTime, oldEndTime, oldLocation, oldDates.
+		#Get New InstructorOfficeHours Attributes:
+		newStartTime = request.data.get("newStartTime")
+		newEndTime = request.data.get("newEndTime")
+		newLocation = request.data.get("newLocation")
+		newDates = request.data.get("newDates")
+		#Filter + Obtain Old Existing InstructorOfficeHours Object.
+		currentOH = InstructorOfficeHours.objects.filter(pk=oldID)
+		#Error Checking For Accuracy of Database Filtering:
+		if(currentOH != 1):
+			return HttpResponse("Error: Specific InstructorOfficeHours Does Not Exist.", content_type="text/plain", status=403)
+		#Set New Values To Update Database Object:
+		#Dependency on Frontend To Check For Accuracy In Fields:
+		currentOH.meetStartTime = newStartTime
+		currentOH.meetEndTime = newEndTime
+		currentOH.meetLocation = newLocation
+		currentOH.meetDates = newDates
+		#Note: MeetInstructor Cannot Change.
+		#Save Changes To Database Object:
+		currentOH.save();
+		return HttpResponse("Sucessfully Updated InstructorOfficeHours!", content_type="text/plain", status=200)
 		
