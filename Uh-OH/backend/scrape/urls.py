ecc5d173dd9a98696df2700651b0c52f290dbfcf
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-04-11 16:20:03 -0400

    Added Support To Query Database By Instructor Email To Send Instructor Data + Instructor Office Hours Data To Frontend.

--- Uh-OH/backend/scrape/urls.py
+++ Uh-OH/backend/scrape/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-	path('courses/',views.CourseAPIView.as_view())
-	#URL Path For Updating Office Hours 
-	path('update/', views.UpdateOHAPIView.as_view())
+	path('courses/',views.CourseAPIView.as_view()),
+	path('hours/', views.InstructorAPIView.as_view())
 ]
