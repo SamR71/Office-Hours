c87b8e0a59044cc6b2490b04d97a455775351f4b
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-04-11 22:59:16 -0400

    Added Comma For Schedules URLS File.

--- Uh-OH/backend/schedules/urls.py
+++ Uh-OH/backend/schedules/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('add/', views.addOH, name='addOH'),
-    path('remove/', views.removeOH, name='removeOH')
+    path('remove/', views.removeOH, name='removeOH'),
     path('get/', views.getSchedule, name='getSchedule'),
-    path('update/', views.getSchedule, name='updateSchedules'),
+    path('update/', views.getSchedule, name='updateSchedules')
 ]
