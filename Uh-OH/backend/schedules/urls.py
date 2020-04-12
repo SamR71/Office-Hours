5f1874aa73620ceb0844c4c32d9f1110f9955e10
Venkat Srinivas <venkatrsrinivas@gmail.com>
2020-04-11 23:17:53 -0400

    Fix For Error In Schedule URLS.

--- Uh-OH/backend/schedules/urls.py
+++ Uh-OH/backend/schedules/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('add/', views.addOH, name='addOH'),
     path('remove/', views.removeOH, name='removeOH'),
     path('get/', views.getSchedule, name='getSchedule'),
-    path('update/', views.getSchedule, name='updateSchedules')
+    path('update/', views.updateSchedules, name='updateSchedules')
 ]
